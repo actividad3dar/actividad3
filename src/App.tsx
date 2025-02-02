7854b08b41433950a4529377d1feb1aa577f406d
actividad3dar <actividad3dar@gmail.com>
2025-02-02 12:47:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import useGeolocation from "./useGeolocation";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { location, error } = useGeolocation();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Encuentra Gasolineras Cercanas</h1>
+      
+      {error && <p style={{ color: "red" }}>{error}</p>}
 
-export default App
+      {location ? (
+        <p>
+          🌍 Tu ubicación: <br />
+          📍 Latitud: <strong>{location.lat}</strong> <br />
+          📍 Longitud: <strong>{location.lon}</strong>
+        </p>
+      ) : (
+        <p>Obteniendo ubicación...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
