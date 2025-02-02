6f31a20ec5995bed957b982f85c2a6f650edf939
actividad3dar <actividad3dar@gmail.com>
2025-02-02 18:26:08 +0100

    Update gasolineras.ts

--- src/api/gasolineras.ts
+++ src/api/gasolineras.ts
@@ -5,9 +5,10 @@ const API_URL = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburante
 export const obtenerGasolineras = async () => {
   try {
     const response = await axios.get(`${API_URL}EstacionesTerrestres/`);
-    return response.data.ListaEESSPrecio; // Array con las gasolineras
+    console.log("Respuesta de la API:", response.data);
+    return response.data.ListaEESSPrecio; // Verifica que esta clave existe
   } catch (error) {
     console.error("Error al obtener los datos de la API:", error);
     return [];
   }
-}; 
+};
